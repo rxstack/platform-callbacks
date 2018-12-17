cc09ba8da64d161cdad3eaa2da86a90db219cc0a
Nikolay Georgiev <n.georgiev@testbirds.de>
2018-12-17 18:41:13 +0200

    reverted some code

--- src/restrict-to-authenticated-user.ts
+++ src/restrict-to-authenticated-user.ts
@@ -7,23 +7,19 @@ import {assertToken} from './utils';
 
 export const restrictToAuthenticatedUser = (fullyAuthenticated = true): ApiOperationCallback => {
   return async (event: ApiOperationEvent): Promise<void> => {
-    validateEventTypeOperation(event.eventType);
+    const operations = [
+      OperationEventsEnum.PRE_COLLECTION_READ,
+      OperationEventsEnum.PRE_WRITE,
+      OperationEventsEnum.PRE_READ,
+      OperationEventsEnum.PRE_REMOVE,
+    ];
+    if (!operations.includes(event.eventType)) {
+      throw new MethodNotAllowedException(`EventType ${event.eventType} is not supported.`);
+    }
     const token = event.request.token;
     assertToken(token);
     if (!token.isAuthenticated() || (fullyAuthenticated && !token.isFullyAuthenticated())) {
       throw new UnauthorizedException();
     }
   };
-};
-
-const validateEventTypeOperation = (eventType: OperationEventsEnum): void => {
-  const operations = [
-    OperationEventsEnum.PRE_COLLECTION_READ,
-    OperationEventsEnum.PRE_WRITE,
-    OperationEventsEnum.PRE_READ,
-    OperationEventsEnum.PRE_REMOVE,
-  ];
-  if (!operations.includes(eventType)) {
-    throw new MethodNotAllowedException(`EventType ${eventType} is not supported.`);
-  }
 };
--- src/restrict-to-role.ts
+++ src/restrict-to-role.ts
@@ -7,23 +7,19 @@ import {assertToken} from './utils';
 
 export const restrictToRole = (role: string): ApiOperationCallback => {
   return async (event: ApiOperationEvent): Promise<void> => {
-    validateEventTypeOperation(event.eventType);
+    const operations = [
+      OperationEventsEnum.PRE_WRITE,
+      OperationEventsEnum.PRE_COLLECTION_READ,
+      OperationEventsEnum.PRE_READ,
+      OperationEventsEnum.PRE_REMOVE,
+    ];
+    if (!operations.includes(event.eventType)) {
+      throw new MethodNotAllowedException(`EventType ${event.eventType} is not supported.`);
+    }
     const token = event.request.token;
     assertToken(token);
     if (!token.hasRole(role)) {
       throw new ForbiddenException();
     }
   };
-};
-
-const validateEventTypeOperation = (eventType: OperationEventsEnum): void => {
-  const operations = [
-    OperationEventsEnum.PRE_WRITE,
-    OperationEventsEnum.PRE_COLLECTION_READ,
-    OperationEventsEnum.PRE_READ,
-    OperationEventsEnum.PRE_REMOVE,
-  ];
-  if (!operations.includes(eventType)) {
-    throw new MethodNotAllowedException(`EventType ${eventType} is not supported.`);
-  }
 };
