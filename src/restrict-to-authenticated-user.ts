4ace632c3fcd755129a03f693b198d1d9eb54b01
Nikolay Georgiev <n.georgiev@testbirds.de>
2018-12-17 18:45:57 +0200

    fixes

--- src/restrict-to-authenticated-user.ts
+++ src/restrict-to-authenticated-user.ts
@@ -7,19 +7,23 @@ import {assertToken} from './utils';
 
 export const restrictToAuthenticatedUser = (fullyAuthenticated = true): ApiOperationCallback => {
   return async (event: ApiOperationEvent): Promise<void> => {
-    const operations = [
-      OperationEventsEnum.PRE_COLLECTION_READ,
-      OperationEventsEnum.PRE_WRITE,
-      OperationEventsEnum.PRE_READ,
-      OperationEventsEnum.PRE_REMOVE,
-    ];
-    if (!operations.includes(event.eventType)) {
-      throw new MethodNotAllowedException(`EventType ${event.eventType} is not supported.`);
-    }
+    validateEventTypeOperation(event.eventType);
     const token = event.request.token;
     assertToken(token);
     if (!token.isAuthenticated() || (fullyAuthenticated && !token.isFullyAuthenticated())) {
       throw new UnauthorizedException();
     }
   };
+};
+
+const validateEventTypeOperation = (eventType: OperationEventsEnum): void => {
+  const operations = [
+    OperationEventsEnum.PRE_COLLECTION_READ,
+    OperationEventsEnum.PRE_WRITE,
+    OperationEventsEnum.PRE_READ,
+    OperationEventsEnum.PRE_REMOVE,
+  ];
+  if (!operations.includes(eventType)) {
+    throw new MethodNotAllowedException(`EventType ${eventType} is not supported.`);
+  }
 };
