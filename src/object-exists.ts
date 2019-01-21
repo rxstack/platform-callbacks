87a3100c29827d37197df38821cb71fba3ed0d7a
Nikolay Georgiev <n.georgiev@testbirds.de>
2019-01-21 18:17:28 +0200

    refactored object-exists

--- src/object-exists.ts
+++ src/object-exists.ts
@@ -10,32 +10,16 @@ import {ObjectExistSchema} from './interfaces';
 export const objectExists = <T>(schema: ObjectExistSchema<T>): OperationCallback => {
   return async (event: OperationEvent): Promise<void> => {
     restrictToOperations(event.eventType, [OperationEventsEnum.PRE_EXECUTE]);
-    await processData(event.request.body, schema, event.injector);
+    await processData(objectExistsItem, event.request.body, schema, event.injector);
   };
-};
-
-const processData = async <T>(input: any, schema: ObjectExistSchema<T>, injector: Injector) => {
-  if (_.isArray(input)) {
-    for (let i = 0; i < input.length; i++) {
-      await objectExistsItem(input[i], schema, injector);
-    }
-  } else {
-    await objectExistsItem(input, schema, injector);
-  }
 };
 
 const objectExistsItem = async <T>(source: Object, schema: ObjectExistSchema<T>, injector: Injector): Promise<void> => {
   const data = schema.dataPath ? getProperty(source, schema.dataPath) : source;
-  if (_.isArray(data)) {
-    for (let i = 0; i < data.length; i++) {
-      await validate(data[i], schema, injector);
-    }
-  } else {
-    await validate(data, schema, injector);
-  }
+  await processData(validateObject, data, schema, injector);
 };
 
-const validate = async <T>(data: Object, schema: ObjectExistSchema<T>, injector: Injector): Promise<void> => {
+const validateObject = async <T>(data: Object, schema: ObjectExistSchema<T>, injector: Injector): Promise<void> => {
   const id = getProperty(data, schema.parentField);
   const service = injector.get(schema.service);
   const defaultCriteria = {[schema.childField]: {'$eq': id}};
@@ -44,6 +28,16 @@ const validate = async <T>(data: Object, schema: ObjectExistSchema<T>, injector:
   const result = await service[method](criteria);
   if (!result) {
     throw new BadRequestException('Resource does not exist');
+  }
+};
+
+const processData = async <T>(func: Function, input: any, schema: ObjectExistSchema<T>, injector: Injector) => {
+  if (_.isArray(input)) {
+    for (let i = 0; i < input.length; i++) {
+      await func(input[i], schema, injector);
+    }
+  } else {
+    await func(input, schema, injector);
   }
 };
 
